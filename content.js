415779ef4febb5de6829f4091f7b9317534af542
rustynull <>
2024-06-12 20:07:02 +0300

    Mobile

--- content.js
+++ content.js
@@ -1,6 +1,7 @@
 (() => {
     // original symbols saved, Map{position: symbol}
-    let SAVED = new Map();
+    let SAVED_D = new Map();
+    let SAVED_M = new Map();
 
     console.log('[C] Start')
     if (window.hasRun) {
@@ -18,6 +19,8 @@
         let min = m[0][1]
         return m.filter((el) => el[1] === min)
     }
+
+    // DESKTOP
 
     // Highlight words
     function highlight_desktop(words){
@@ -54,7 +57,7 @@
 
         symbols.forEach((span, i)=>{
             span.style.backgroundColor="green";
-            SAVED.set(i2+i, symbols[0].textContent);
+            SAVED_D.set(i2+i, symbols[0].textContent);
         });
 
         symbols[0].textContent = "(";
@@ -70,32 +73,67 @@
             document.querySelectorAll(".cursor-pointer span")
         );
 
-        symbols.forEach(span=>{span.style = ""});
+        symbols.forEach(span=>{span.style.backgroundColor="";});
 
-        SAVED.forEach((sym, i)=>{
+        SAVED_D.forEach((sym, i)=>{
             symbols[i].textContent = sym; 
         });
-        SAVED.clear();
+        SAVED_D.clear();
+    }
+
+    // MOBILE
+    function highlight_mobile(words){
+        words.forEach(([word, cost]) => highlight_word_mobile(word, cost));
+    }
+
+    function highlight_word_mobile(word, cost){
+        // div element
+        let div = Array.from(document.querySelectorAll('.text-base.lowercase'))
+        .find(el=>el.textContent == word);
+        if (div == undefined) return;    
+
+        let cost_div = document.createElement('div');
+        cost_div.textContent = `(${cost})`;
+        div.after(cost_div);
+
+        div.style.backgroundColor="green";
+    }
+
+    function clear_highlight_mobile(){
+        console.log("[C] clear highlight")
+        document.querySelectorAll('.text-base.lowercase')
+        .forEach(el=>{
+            if (el.nextSibling) {
+                el.nextSibling.remove();
+            }
+            el.style.backgroundColor = "";
+        });
     }
 
     function is_desktop(){
-        return true;
+        return Array.from(
+            document.querySelectorAll(".cursor-pointer span")
+        ).length > 300;
     }
 
-    browser.runtime.onMessage.addListener((m) => {
-        console.log(`[C] Get ${m}`);
-        // Get min cost life elements
-        if (m.length !== 0){
-            if (is_desktop()){
-                clear_highlight_desktop()
-                let min = filter_min_cost(m);
-                console.log(`[C] ${min}`)
-                highlight_desktop(min);
-            }
- 
-
-        } else {
+    browser.runtime.onMessage.addListener(answer => {
+        console.log(`[C] Get ${answer}`);
+        if (answer.length === 0) {
             console.error("[C] empty array solver");
+            return
+        };
+
+        // Get min cost life elements
+        let answer_min = filter_min_cost(answer);
+        console.log(`[C] ${answer_min}`)
+        if (is_desktop()){
+            console.log("[C] Desktop");
+            clear_highlight_desktop()
+            highlight_desktop(answer_min);
+        } else{
+            console.log("[C] Mobile");
+            clear_highlight_mobile();
+            highlight_mobile(answer_min);
         }
     });
 })();

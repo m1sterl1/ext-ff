ce40d7911ad97846109c3d9a206af3c2c3f238e9
rustynull <>
2024-08-20 20:54:00 +0300

    Event listener replaced

--- popup/reload.js
+++ popup/reload.js
@@ -1,16 +1,7 @@
 
 function listenForClicks() {
     console.log("Listen for clicks")
-    document.addEventListener("click", (e) => {
-
-        /**
-        * Just log the error to the console.
-        */
-        function reportError(error) {
-            console.error(`Could not beastify: ${error}`);
-        }
-
- 
+    document.addEventListener("click", (e) => { 
         if (e.target.id === "reload_ext") {
             console.log("Reloading");
             browser.runtime.reload();
